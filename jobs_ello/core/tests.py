032886ac4d48e237210fa52add6d1646b2db1e7a
elton jefferson <eltonjncorreia@gmail.com>
2018-03-03 20:02:33 -0300

    refactoring

--- jobs_ello/core/tests.py
+++ jobs_ello/core/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
 
-from jobs_ello.core.views import salvar_dados
+from jobs_ello.core.views import salvar_dados, buscar_parlamentares
 
 
 class HomeTest(TestCase):
@@ -12,10 +12,17 @@ class HomeTest(TestCase):
         """GET / return status code 200"""
         self.assertEqual(200, self.resp.status_code)
 
-    def test_json(self):
-        self.assertTrue(self.resp, salvar_dados())
-
     def test_template(self):
         """Esta usando o template core/index.html"""
         self.assertTemplateUsed(self.resp, 'core/index.html')
+
+    def test_existe_funcao_salvar_dados(self):
+        """testa se existe uma funcao chamanda salvar dados"""
+        self.assertTrue(self.resp, salvar_dados)
+
+    def test_existe_funcao_buscar_parlamentar(self):
+        """se existir uma funcao buscar parlamentares"""
+        self.assertTrue(self.resp, buscar_parlamentares)
+
+
 
